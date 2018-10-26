c5b3cc14bcf4315ce61813c0fc6339250acee4bc
Pulkit Garg <pulkitgarg6776@gmail.com>
2018-10-26 14:07:48 +0530

    Adaptive GridView as per Screen Size

--- app/src/main/java/cic/du/ac/in/recylerviewandcardview/HomePage.java
+++ app/src/main/java/cic/du/ac/in/recylerviewandcardview/HomePage.java
@@ -1,10 +1,13 @@
 package cic.du.ac.in.recylerviewandcardview;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,21 @@ public class HomePage extends AppCompatActivity {
 
     List<Book> lstBook ;
 
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int noOfColumns = (int) (dpWidth / 180);
+        return noOfColumns;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("BookStore");
 
         lstBook = new ArrayList<>();
         lstBook.add(new Book("The Vegitarian","Categorie Book","Description book",R.drawable.thevigitarian));
@@ -39,7 +53,7 @@ public class HomePage extends AppCompatActivity {
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerView);
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,lstBook);
-        myrv.setLayoutManager(new GridLayoutManager(this,3));
+        myrv.setLayoutManager(new GridLayoutManager(this,calculateNoOfColumns(this)));
         myrv.setAdapter(myAdapter);
     }
 }
--- app/src/main/java/cic/du/ac/in/recylerviewandcardview/RecyclerViewAdapter.java
+++ app/src/main/java/cic/du/ac/in/recylerviewandcardview/RecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package cic.du.ac.in.recylerviewandcardview;
 
 import android.content.Context;
-import android.media.Image;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import java.util.List;
 
@@ -33,7 +31,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
         View view ;
         LayoutInflater mInflater = LayoutInflater.from(mContext);
         view = mInflater.inflate(R.layout.item_view,parent,false);
@@ -42,19 +39,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
-
         holder.tv_book_title.setText(mData.get(position).getTitle());
         holder.img_book_thumbnail.setImageResource(mData.get(position).getThumbnail());
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(mContext, "tapped", Toast.LENGTH_SHORT).show();
-
             }
         });
-
-
-
     }
 
     @Override
@@ -63,14 +55,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-
         TextView tv_book_title;
         ImageView img_book_thumbnail;
         CardView cardView ;
 
         public MyViewHolder(View itemView) {
             super(itemView);
-
             tv_book_title = (TextView) itemView.findViewById(R.id.book_name) ;
             img_book_thumbnail = (ImageView) itemView.findViewById(R.id.book_img);
             cardView = (CardView) itemView.findViewById(R.id.cardview);
